06d7d92571e49703f3de4dafcc1e5dd830c207e0
AnanthuMuralee <ananthakrishnan432001@gmail.com>
2023-01-22 15:56:27 +0530

    added: css for mess_preference

--- mess.py
+++ mess.py
@@ -43,11 +43,7 @@ def index():
 			if user == []:
 				flash("Username&Password not found!!!")
 			else:
-				user = get_post_student(username)
-				if user['hostel'] is None:
-					return redirect(url_for('create_profile',username=username))
-				else:
-					return redirect(url_for('show_profile',username=username))
+				return redirect(url_for('profile',username=username))
 		else:
 			conn = connect_db()
 			user = conn.execute("SELECT id FROM admin WHERE username = ? AND pwd = ?",(username,pwd)).fetchall()
@@ -57,86 +53,23 @@ def index():
 				return redirect(url_for('admin',username=username))
 	return render_template('index.html')
 	
-@app.route('/<username>/create_profile',methods=('GET','POST'))
-def create_profile(username):
-
-	if request.method == 'POST':
-		if request.form['action'] == 'save':
-			gender = request.form['gender']
-			hostel = request.form['hostel']
-			room = request.form['room']
-			if gender == "None" or hostel == "None" or room == "None":
-				flash("Fill the options and Click Save Changes to Continue")
-			else:	
-				conn = connect_db()
-				conn.execute("UPDATE student SET gender = ?, hostel = ?, room = ? WHERE username = ?",(gender,hostel,room,username))
-				conn.commit()
-				conn.close()
-				return redirect(url_for('show_profile',username=username))		
-
+@app.route('/<username>/profile')
+def profile(username):
 	user = get_post_student(username)
 	name=user['name']
 	fname=name.split()
 	email=fname[0].lower()+'_'+username.lower()+'@nitc.ac.in'
 	return render_template('profile_student.html',user=user,email=email)
-	
-@app.route('/<username>/profile',methods=('GET','POST'))
-def show_profile(username):
-	user = get_post_student(username)
-	name=user['name']
-	fname=name.split()
-	email=fname[0].lower()+'_'+username.lower()+'@nitc.ac.in'
-	if request.method == 'POST':
-		if request.form['action'] == 'continue':
-			return redirect(url_for('mess_preference',username=username))
-	return render_template('show_profile.html',user=user,email=email)
-	
+
 @app.route('/<username>/profile/mess_preference')
 def mess_preference(username):
 	user = get_post_student(username)
-	
+	if user['gender'] is None or user['hostel'] is None or user['room'] is None:
+		flash("Fill the option to continue or Click Save Changes to Continue")
+		return redirect(url_for('profile',username=username))
 	return render_template('mess_preference.html',user=user)
-
+	
 @app.route('/<username>/admin')
 def admin(username):
 	user = get_post_admin(username)
 	return render_template('admin.html',user=user)
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
